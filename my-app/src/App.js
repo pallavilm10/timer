f505fb972dbad12ac5c279ddeb7b3d5cc8f116bc
pallavilm10 <111088372+pallavilm10@users.noreply.github.com>
2023-12-13 13:18:07 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import { FaPlay, FaPause, FaStop } from 'react-icons/fa';
+import './countdown.css';
+
+const Countdown = () => {
+
+  
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        if (minutes === 0 && seconds === 0) {
+          clearInterval(interval);
+          setIsActive(false);
+        } else {
+          if (seconds === 0) {
+            setMinutes((prevMinutes) => prevMinutes - 1);
+            setSeconds(59);
+          } else {
+            setSeconds((prevSeconds) => prevSeconds - 1);
+          }
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, minutes, seconds]);
+
+  const handleStart = () => {
+    setIsActive(true);
+  };
+
+  const handlePause = () => {
+    setIsActive(false);
+  };
+
+  const handleStop = () => {
+    setIsActive(false);
+    setMinutes(0);
+    setSeconds(0);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setMinutes(0);
+    setSeconds(0);
+  };
+
+  const formatTime = (time) => {
+    return String(time).padStart(2, '0');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    
+    <div className="container">
+    <div className="floating-text">COUNTDOWN-TIMER</div>
+    <span style={{ fontSize: '48px',color: '#3d73ad' }}> Minutes</span>
+      <div>
+        <input
+          type="number"
+          value={minutes}
+          onChange={(e) => setMinutes(parseInt(e.target.value, 10))}
+          style={{ fontSize: '36px' }}
+        />
+        
+      </div>
+      <div>
+        <p style={{ fontSize: '40px',fontWeight: 'bold', color: '#87CEEB' }}>
+          {formatTime(Math.floor(minutes / 60))}:{formatTime(minutes % 60)}:
+          {formatTime(seconds)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div>
+        <button onClick={handleStart} disabled={isActive}>
+          <FaPlay />
+        </button>
+        <button onClick={handlePause} disabled={!isActive}>
+          <FaPause />
+        </button>
+        <button onClick={handleStop} disabled={!isActive}>
+          <FaStop />
+        </button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Countdown;
+
